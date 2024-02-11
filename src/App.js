d3189dafdb8a3e45d392611ad348d33a71a7f62a
Sayan Guha <92387570+Sayan625@users.noreply.github.com>
2024-02-11 18:25:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+// Import CSS file and necessary components from react-router-dom
 import './App.css';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
+
+// Import page components
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 
-
+// Define the main App component
 function App() {
   return (
+    // Main container
     <div className="">
+      {/* Router component to enable routing */}
       <Router>
+        {/* Routes component to define routes */}
         <Routes>
+          {/* Route for the profile page */}
           <Route path="/profile" element={<Profile />} />
+          {/* Route for the main page */}
           <Route path="/" element={<Main />} />
+          {/* Route for the login page */}
           <Route path="/login" element={<Login />} />
+          {/* Route for the signup page */}
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </Router>
@@ -25,4 +35,5 @@ function App() {
   );
 }
 
+// Export the App component
 export default App;
